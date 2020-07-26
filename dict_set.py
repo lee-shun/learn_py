1c54b08ac1909fcb50851fd332f804fd82cb40c1
lee-shun <lee970802@163.com>
2020-07-26 09:40:12 +0800

    one

---
+++ dict_set.py
@@ -0,0 +1,8 @@
+# dict实际上是字典：map映射--->(key--->value)
+dic = {'shun':95,'lee':86}
+print(dic)
+print(dic['shun'])
+
+#set和数学上的集合的意义一致
+s=set([1,2,2,2,2,23,3,])
+print(s)
---
+++ list_tuple.py
@@ -0,0 +1,19 @@
+#list 用[]来表示
+classmates = ['lee','shun','niu','pi']
+print(classmates)
+classmates.append('hhhh')
+print(classmates)
+
+#tuple 使用（）表示：
+teachers = ('waaaaang','zhang','sun')
+print(teachers)
+
+# -*- coding: utf-8 -*-
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+# 打印Apple:
+print(L[ 0 ][ 0 ])
